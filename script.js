e38f47f3e239dd8a189c54c0d3a7c0f5bc37cf0f
Aayush Bharti <aayushbh2002@gmail.com>
2024-01-30 17:50:38 +0530

    update

---
+++ script.js
@@ -0,0 +1,10 @@
+const boxes = document.querySelectorAll(".box");
+const gameInfo = document.querySelectorAll(".game-info");
+const newGameBtn = document.querySelectorAll(".btn");
+
+let currentPlayer;
+let gameGrid;
+
+const winingPosition = {
+    []
+};
