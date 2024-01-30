6c35890477373fd290af68877a96f6a96d7a7fc7
Aayush Bharti <aayushbh2002@gmail.com>
2024-01-30 19:04:26 +0530

    update

--- script.js
+++ script.js
@@ -1,10 +1,42 @@
 const boxes = document.querySelectorAll(".box");
-const gameInfo = document.querySelectorAll(".game-info");
-const newGameBtn = document.querySelectorAll(".btn");
+const gameInfo = document.querySelector(".game-info");
+const newGameBtn = document.querySelector(".btn");
 
 let currentPlayer;
 let gameGrid;
 
-const winingPosition = {
-    []
-};
+const winningPositions = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+
+//Afunction to initialise the game
+function initGame() {
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    newGameBtn.classList.remove("active");
+    gameInfo.innerText = `Current Player - ${currentPlayer}`;
+
+    //Grid initially empty
+    boxes.forEach((box, index) => {
+        box.innerText = "";
+        boxes[index].style.pointerEvents = "all";
+        //one more thing is missing, initialise box with css properties again
+        box.classList = `box box${index+1}`;
+    });
+}
+
+initGame();
+
+boxes.forEach((box,index) =>{
+    box.addEventListener("click", () => {
+        handleClick(index);
+    })
+});
